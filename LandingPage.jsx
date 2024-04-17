7de17adfc716092f194ab6f6ee7e104e49a24511
jdorsey44 <159090126+jdorsey44@users.noreply.github.com>
2024-04-17 11:32:20 -0700

    Add files via upload

---
+++ LandingPage.jsx
@@ -0,0 +1,15 @@
+const LandingPage = ({ currentUser, onLogin }) => {
+  return (
+    <div className="landingpage">
+      <h1>Welcome to my React!</h1>
+      {currentUser.name !== "John" ? (
+        <p>Hello, {John.Dorsey}!</p>
+      ) : (
+        <p>Please log in to access more features.</p>
+      )}
+      <button onClick={onLogin}>Login</button>
+    </div>
+  );
+};
+
+export default LandingPage; 
